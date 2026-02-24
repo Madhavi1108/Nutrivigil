2cb89eb6cfcb516a1a49ee59b0477090f45b6d38
Gagan021-5 <gagansingh010221@gmail.com>
2026-02-24 19:00:03 +0530

    first

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,28 +1,3 @@
-// import multer from "multer";
-// import fs from "fs";
-// import path from "path";
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     const uploadDir = path.join(process.cwd(), "uploads"); 
-
-//     if (!fs.existsSync(uploadDir)) {
-//       fs.mkdirSync(uploadDir, { recursive: true });
-//     }
-
-//     cb(null, uploadDir);
-//   },
-//   filename: (req, file, cb) => {
-//     const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     cb(null, unique + path.extname(file.originalname));
-//   },
-// });
-
-
-// export default multer({
-//   storage,
-//   limits: { fileSize: 10 * 1024 * 1024 },
-// });
 import multer from "multer";
 import fs from "fs";
 import path from "path";
